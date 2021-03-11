80c94801637573f4b0fc85c214d73860f0693812
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2021-03-11 22:23:26 +0100

    Add project files.

---
+++ RestBeer/Controllers/BeersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestBeer.Managers;
+using BeerLib;
+
+namespace RestBeer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeersController : Controller
+    {
+        private readonly BeersManagers _manager = new BeersManagers();
+
+
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IEnumerable<Beer> Get([FromQuery] string name, [FromQuery] string sort_by)
+        {
+            return _manager.GetAll(name, sort_by);
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Beer> Get(int id)
+        {
+            Beer beer = _manager.GetById(id);
+            if (beer == null) return NotFound("No such beer, id: " + id);
+            return Ok(beer);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Beer> Post([FromBody] Beer value)
+        {
+            try
+            {
+                Beer newBeer = _manager.Add(value);
+                string uri = Url.RouteUrl(RouteData.Values) + "/" + newBeer.ID;
+                return Created(uri, newBeer);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Beer> Put(int id, [FromBody] Beer value)
+        {
+            try
+            {
+                Beer updatedBeer = _manager.Update(id, value);
+                if (updatedBeer == null) return NotFound("No such beer, id: " + id);
+                return Ok(updatedBeer);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Beer> Delete(int id)
+        {
+            Beer deletedBeer = _manager.Delete(id);
+            if (deletedBeer == null) return NotFound("No such beer, id: " + id);
+            return Ok(deletedBeer);
+        }
+    }
+}
---
+++ RestBeer/Managers/BeersManagers.cs
@@ -0,0 +1,75 @@
+﻿using BeerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestBeer.Managers
+{
+    public class BeersManagers
+    {
+        private static int _nextId = 1;
+        private static readonly List<Beer> Data = new List<Beer>
+        {
+            new Beer("Turbog", 74, 4.6) {ID = _nextId++},
+            new Beer("Gobrut", 47, 6.4) {ID = _nextId++},
+            new Beer("Carlsberg", 73, 5) {ID = _nextId++}
+        };
+
+        public List<Beer> GetAll(string name = null, string sortBy = null)
+        {
+            List<Beer> beers = new List<Beer>(Data);
+
+            if (name != null)
+            {
+                beers = beers.FindAll(beer => beer.Name.StartsWith(name));
+            }
+            if (sortBy != null)
+            {
+                switch (sortBy.ToLower())
+                {
+                    case "name":
+                        beers = beers.OrderBy(beer => beer.Name).ToList();
+                        break;
+                    case "price":
+                        beers = beers.OrderBy(beer => beer.Price).ToList();
+                        break;
+                    case "adv":
+                        beers = beers.OrderBy(beer => beer.Abv).ToList();
+                        break;
+                }
+            }
+            return beers;
+        }
+
+        public Beer GetById(int id)
+        {
+            return Data.Find(beer => beer.ID == id);
+        }
+
+        public Beer Add(Beer newBeer)
+        {
+            newBeer.ID = _nextId++;
+            Data.Add(newBeer);
+            return newBeer;
+        }
+
+        public Beer Delete(int id)
+        {
+            Beer beer = Data.Find(beer1 => beer1.ID == id);
+            if (beer == null) return null;
+            Data.Remove(beer);
+            return beer;
+        }
+
+        public Beer Update(int id, Beer updates)
+        {
+            Beer beer = Data.Find(beer1 => beer1.ID == id);
+            if (beer == null) return null;
+            beer.Name = updates.Name;
+            beer.Price = updates.Price;
+            beer.Abv = updates.Abv;
+            return beer;
+        }
+    }
+}
